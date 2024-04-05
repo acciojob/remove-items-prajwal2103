111333cdd191936ee7352a73b44126b0d41be9d3
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-04-05 22:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let btn=document.querySelector("input");
+btn.addEventListener("click",remove);
+function remove(){
+    let select = document.getElementById("colorSelect");
+    // Get the selected option's index
+    let selectedIndex = select.selectedIndex;
+    let selectedOption = select.options[selectedIndex];
+    selectedOption.remove();
+}
